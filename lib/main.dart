37da8a54ec9718a168bd403dcf359c26ad45097e
Md Mamun Hossain <wmamun690@gmail.com>
2026-09-08 21:33:08 +0600

    Add deposit and withdraw methods

--- lib/main.dart
+++ lib/main.dart
@@ -6,4 +6,22 @@ class BankAccount {
   BankAccount(this.accountHolderName, this.accountNumber, this._balance);
 
   double get balance => _balance;
+
+  void deposit(double amount) {
+    if (amount > 0) {
+      _balance += amount;
+    } else {
+      print('Deposit amount must be positive.');
+    }
+  }
+
+  void withdraw(double amount) {
+    if (amount <= 0) {
+      print('Withdrawal amount must be positive.');
+    } else if (amount > _balance) {
+      print('Insufficient balance.');
+    } else {
+      _balance -= amount;
+    }
+  }
 }
